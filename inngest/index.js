fab5ba7380db726094cc7bf526415ff2074ee0c5
Hemachaudhary19 <hemac1872@gmail.com>
2025-09-29 11:51:24 +0530

    first commit

---
+++ inngest/index.js
@@ -0,0 +1,7 @@
+import { Inngest } from "inngest";
+
+// create inngest client
+export const inngest = new Inngest({ name: "Quick Movie Show" });
+
+// define functions (empty for now, add later)
+export const functions = [];
